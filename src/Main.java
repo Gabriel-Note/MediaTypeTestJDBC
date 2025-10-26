8bea6f7bbd399eafdf85976bbd615feaa1c7e3b9
gabriel note <gabriel.emil.note@gmail.com>
2025-10-26 20:46:57 +0100

    bara ändringar för att testa saker

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
         BookRepository br = new BookRepository();
         MemberRepository mr = new MemberRepository();
         LoanRepository lr = new LoanRepository();
         MediaRepository mediaRepository = new MediaRepository();
+        MemberRepository memberRepository = new MemberRepository();
+
+//        memberRepository.updateMember(1, "", "Svensson@sturemail.com", "      ");
+        memberRepository.updateMember(1, "Erik Andersson", "erik.andersson@email.se", "Stockholm");
+        memberRepository.updateMember(10, "Linda Pettersson", "linda.pettersson@email.se", "Jönköping");
         Menu menu = new Menu();
         menu.mainSelection();
 
